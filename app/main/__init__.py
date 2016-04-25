dce556dbada2f0273ac648946cc94947cca598a1
wanghaa <wanghaa@gmail.com>
2016-04-26 01:51:57 +0800

    增加用户角色（9a）

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,5 +1,12 @@
 from flask import Blueprint
 
+from app.models import Permission
+
 main = Blueprint('main', __name__)
 
 from . import views, errors
+
+
+@main.app_context_processor
+def inject_context_permissions():
+    return dict(Permission=Permission)
