964e395a3d78434fd0f72d333a1f33db06a6186d
wanghaa <wanghaa@gmail.com>
2016-05-05 16:05:23 +0800

    用户模型增加关注者和关注（12a）

--- app/models.py
+++ app/models.py
@@ -50,6 +50,15 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 
+class Follow(db.Model):
+    __tablename__ = 'follows'
+    follower_id = db.Column(db.Integer, db.ForeignKey('users.id'),
+                            primary_key=True)
+    followed_id = db.Column(db.Integer, db.ForeignKey('users.id'),
+                            primary_key=True)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -65,128 +74,154 @@ class User(UserMixin, db.Model):
     last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
     avatar_hash = db.Column(db.String(32))
     posts = db.relationship('Post', backref="author", lazy='dynamic')
+    followed = db.relationship('Follow',
+                               foreign_keys=[Follow.follower_id],
+                               backref=db.backref('follower', lazy='joined'),
+                               lazy='dynamic',
+                               cascade='all, delete-orphan')
+    followers = db.relationship('Follow',
+                                foreign_keys=[Follow.followed_id],
+                                backref=db.backref('followed', lazy='joined'),
+                                lazy='dynamic',
+                                cascade='all, delete-orphan')
 
-    @staticmethod
-    def generate_fake(count=100):
-        from sqlalchemy.exc import IntegrityError
-        from random import seed
-        import forgery_py
 
-        seed()
-        for i in range(count):
-            u = User(email=forgery_py.internet.email_address(),
-                     username=forgery_py.internet.user_name(True),
-                     password=forgery_py.lorem_ipsum.word(),
-                     confirmed=True,
-                     name=forgery_py.name.full_name(),
-                     location=forgery_py.address.city(),
-                     about_me=forgery_py.lorem_ipsum.sentence(),
-                     member_since=forgery_py.date.date(True))
-            db.session.add(u)
-            try:
-                db.session.commit()
-            except IntegrityError:
-                db.session.rollback()
+@staticmethod
+def generate_fake(count=100):
+    from sqlalchemy.exc import IntegrityError
+    from random import seed
+    import forgery_py
 
-    def __init__(self, **kwargs):
-        super(User, self).__init__(**kwargs)
-        if self.role is None:
-            if self.email == current_app.config['FLASKY_ADMIN']:
-                self.role = Role.query.filter_by(permissions=0xff).first()
-            else:
-                self.role = Role.query.filter_by(default=True).first()
-        if self.email is not None and self.avatar_hash is None:
-            self.avatar_hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
-
-    @property
-    def password(self):
-        raise AttributeError('密码不可写！')
-
-    @password.setter
-    def password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def verify_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-    def generate_confirmation_token(self, expiration=3600):
-        s = Serializer(current_app.config['SECRET_KEY'], expiration)
-        return s.dumps({'comfirm': self.id})
-
-    def confirm(self, token):
-        s = Serializer(current_app.config['SECRET_KEY'])
+    seed()
+    for i in range(count):
+        u = User(email=forgery_py.internet.email_address(),
+                 username=forgery_py.internet.user_name(True),
+                 password=forgery_py.lorem_ipsum.word(),
+                 confirmed=True,
+                 name=forgery_py.name.full_name(),
+                 location=forgery_py.address.city(),
+                 about_me=forgery_py.lorem_ipsum.sentence(),
+                 member_since=forgery_py.date.date(True))
+        db.session.add(u)
         try:
-            data = s.loads(token)
-        except Exception as e:
-            print(e)
-            return False
-        if data.get('comfirm') != self.id:
-            return False
-        self.confirmed = True
-        db.session.add(self)
-        return True
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
 
-    def generate_reset_token(self, expiration=3600):
-        s = Serializer(current_app.config['SECRET_KEY'], expiration)
-        return s.dumps({'reset': self.id})
 
-    def reset_password(self, token):
-        s = Serializer(current_app.config['SECRET_KEY'])
-        try:
-            data = s.loads(token)
-        except Exception as e:
-            print(e)
-            return False
-        if data.get('reset') != self.id:
-            return False
-        self.confirmed = True
-        db.session.add(self)
-        return True
-
-    def generate_email_change_token(self, new_email, expiration=3600):
-        s = Serializer(current_app.config['SECRET_KEY'], expiration)
-        return s.dumps({'change_email': self.id, 'new_email': new_email})
-
-    def change_email(self, token):
-        s = Serializer(current_app.config['SECRET_KEY'])
-        try:
-            data = s.loads(token)
-        except Exception as e:
-            print(e)
-            return False
-        if data.get('change_email') != self.id:
-            return False
-        new_email = data.get('new_email')
-        if new_email is None:
-            return False
-        if self.query.filter_by(email=new_email).first() is not None:
-            return False
-        self.email = new_email
-        self.avatar_hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
-        db.session.add(self)
-        return True
-
-    def can(self, permissions):
-        return self.role is not None and (self.role.permissions & permissions) == permissions
-
-    def is_administrator(self):
-        return self.can(Permission.ADMINISTER)
-
-    def ping(self):
-        self.last_seen = datetime.utcnow()
-        db.session.add(self)
-
-    def gravatar(self, size=100, default='identicon', rating='g'):
-        if request.is_secure:
-            url = 'https://secure.gravatar.com/avatar'
+def __init__(self, **kwargs):
+    super(User, self).__init__(**kwargs)
+    if self.role is None:
+        if self.email == current_app.config['FLASKY_ADMIN']:
+            self.role = Role.query.filter_by(permissions=0xff).first()
         else:
-            url = 'http://www.gravatar.com/avatar'
-        hash = self.avatar_hash or hashlib.md5(self.email.encode('utf-8')).hexdigest()
-        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(url=url, hash=hash, size=size, default=default,
-                                                                     rating=rating)
+            self.role = Role.query.filter_by(default=True).first()
+    if self.email is not None and self.avatar_hash is None:
+        self.avatar_hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+
+@property
+def password(self):
+    raise AttributeError('密码不可写！')
+
+
+@password.setter
+def password(self, password):
+    self.password_hash = generate_password_hash(password)
+
+
+def verify_password(self, password):
+    return check_password_hash(self.password_hash, password)
+
+
+def generate_confirmation_token(self, expiration=3600):
+    s = Serializer(current_app.config['SECRET_KEY'], expiration)
+    return s.dumps({'comfirm': self.id})
+
+
+def confirm(self, token):
+    s = Serializer(current_app.config['SECRET_KEY'])
+    try:
+        data = s.loads(token)
+    except Exception as e:
+        print(e)
+        return False
+    if data.get('comfirm') != self.id:
+        return False
+    self.confirmed = True
+    db.session.add(self)
+    return True
+
+
+def generate_reset_token(self, expiration=3600):
+    s = Serializer(current_app.config['SECRET_KEY'], expiration)
+    return s.dumps({'reset': self.id})
+
+
+def reset_password(self, token):
+    s = Serializer(current_app.config['SECRET_KEY'])
+    try:
+        data = s.loads(token)
+    except Exception as e:
+        print(e)
+        return False
+    if data.get('reset') != self.id:
+        return False
+    self.confirmed = True
+    db.session.add(self)
+    return True
+
+
+def generate_email_change_token(self, new_email, expiration=3600):
+    s = Serializer(current_app.config['SECRET_KEY'], expiration)
+    return s.dumps({'change_email': self.id, 'new_email': new_email})
+
+
+def change_email(self, token):
+    s = Serializer(current_app.config['SECRET_KEY'])
+    try:
+        data = s.loads(token)
+    except Exception as e:
+        print(e)
+        return False
+    if data.get('change_email') != self.id:
+        return False
+    new_email = data.get('new_email')
+    if new_email is None:
+        return False
+    if self.query.filter_by(email=new_email).first() is not None:
+        return False
+    self.email = new_email
+    self.avatar_hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
+    db.session.add(self)
+    return True
+
+
+def can(self, permissions):
+    return self.role is not None and (self.role.permissions & permissions) == permissions
+
+
+def is_administrator(self):
+    return self.can(Permission.ADMINISTER)
+
+
+def ping(self):
+    self.last_seen = datetime.utcnow()
+    db.session.add(self)
+
+
+def gravatar(self, size=100, default='identicon', rating='g'):
+    if request.is_secure:
+        url = 'https://secure.gravatar.com/avatar'
+    else:
+        url = 'http://www.gravatar.com/avatar'
+    hash = self.avatar_hash or hashlib.md5(self.email.encode('utf-8')).hexdigest()
+    return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(url=url, hash=hash, size=size, default=default,
+                                                                 rating=rating)
+
+
+def __repr__(self):
+    return '<User %r>' % self.username
 
 
 class AnonymousUser(AnonymousUserMixin):
