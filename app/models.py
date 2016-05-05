68f0720c9145e8a93e3e25d12a87061309f33318
wanghaa <wanghaa@gmail.com>
2016-05-05 18:36:44 +0800

    在首页显示所关注用户的文章（12d）

--- app/models.py
+++ app/models.py
@@ -206,7 +206,7 @@ class User(UserMixin, db.Model):
 
     def follow(self, user):
         if not self.is_following(user):
-            f = Follow(follower=self,followed=user)
+            f = Follow(follower=self, followed=user)
             db.session.add(f)
 
     def unfollow(self, user):
@@ -219,6 +219,10 @@ class User(UserMixin, db.Model):
 
     def is_followed_by(self, user):
         return self.followers.filter_by(follower_id=user.id).first() is not None
+
+    @property
+    def followed_posts(self):
+        return Post.query.join(Follow, Follow.followed_id == Post.author_id).filter(Follow.follower_id == self.id)
 
     def __repr__(self):
         return '<User %r>' % self.username
