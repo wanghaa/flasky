a3cda2a6bb0472d9d9cac896029d8091760309c9
wanghaa <wanghaa@gmail.com>
2016-05-05 17:54:32 +0800

    关注和取消关注（12b）

--- config.py
+++ config.py
@@ -17,6 +17,7 @@ class Config:
     FLASKY_MAIL_SENDER = '本来果坊 <wanghaojava@126.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASKY_POSTS_PER_PAGE = 20
+    FLASKY_FOLLOWERS_PER_PAGE = 50
 
     @staticmethod
     def init_app(app):
